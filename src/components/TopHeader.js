d0faf81293160b818ef3c431fa5e2e966da745f8
Ragavi5901 <ragavi5901@gmail.com>
2026-06-12 11:41:59 +0530

    update

--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TopHeader = () => {
   return (
-    <div className="bg-brand-darker text-gray-400 text-[11px] uppercase tracking-widest py-2 px-4 sm:px-6 lg:px-8 flex flex-col md:flex-row justify-between items-center gap-2">
+    <div className="bg-brand-darker text-gray-400 text-[11px] uppercase tracking-widest py-2 px-4 sm:px-6 lg:px-8 hidden md:flex flex-col md:flex-row justify-between items-center gap-2">
       <div className="flex flex-col sm:flex-row items-center gap-2 sm:gap-6 text-center sm:text-left font-semibold">
          <span className="hover:text-white transition-colors cursor-default flex items-center gap-2">
            <svg className="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" /></svg>
